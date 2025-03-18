f7fcc9c58b60796d0aaeb3126e0a541677c80ddd
Eduardopasdf <edugouveia3@gmail.com>
2025-03-18 11:17:27 -0300

    Projeto da Lista de Exercicios

---
+++ ListaExercicio/Ex06/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] vetor = new int[100];
+Random r = new Random();
+for(int i = 0; i < vetor.Length; i++)
+{
+    vetor[i] = r.Next(100);
+}
+
+for (int i = 0; i < vetor.Length - 1; i++)
+{
+    for (int j = 0; j < vetor.Length - 1 - i; j++)
+    {
+        if (vetor[j] > vetor[j + 1])
+        {
+            
+            int temp = vetor[j];
+            vetor[j] = vetor[j + 1];
+            vetor[j + 1] = temp;
+        }
+    }
+}
+
+
+for(int i = 0; i < vetor.Length; i++)
+{
+    Console.Write(vetor[i] + " ");
+}
---
+++ ListaExercicio/Ex06b/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] vetor = new int[100];  // Cria um vetor de inteiros com 100 posições
+        Random r = new Random();     // Instancia um gerador de números aleatórios
+        
+        // Preenche o vetor com números aleatórios de 0 a 99
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            vetor[i] = r.Next(100);
+        }
+
+        // Ordena o vetor em ordem crescente
+        Array.Sort(vetor);
+
+        // Exibe os números ordenados no console
+        foreach (int num in vetor)
+        {
+            Console.Write(num + " ");
+        }
+    }
+}
